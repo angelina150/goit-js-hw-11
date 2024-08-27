75e03864cd154f7257db6fbe7cca95d0a2568d1c
Angelina Chigiryova <chigiryova31@gmail.com>
2024-08-27 13:31:35 +0300

    changed main.js

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import { fetchPhotos } from './js/pixabay-api.js';
 const searchForm = document.querySelector('.js-search-form');
 const userList = document.querySelector('.user-list');
 const loader = document.querySelector('.loader');
+
+const galleryLightbox = new SimpleLightbox('.user-list a', {});
 function showLoader() {
   loader.classList.remove('is-hidden');
 }
@@ -20,8 +22,8 @@ function hideLoader() {
 const onSearchFormSubmit = event => {
   userList.innerHTML = '';
   event.preventDefault();
-  const searchedValue = searchForm.elements.user_query.value;
   showLoader();
+  const searchedValue = searchForm.elements.user_query.value;
   fetchPhotos(searchedValue)
     .then(data => {
       if (data.hits.length === 0) {
@@ -38,11 +40,14 @@ const onSearchFormSubmit = event => {
         .map(imgDetails => createGalleryCardTemplate(imgDetails))
         .join('');
       userList.innerHTML = galleryCardsTemplate;
-      const galleryLightbox = new SimpleLightbox('.user-list a', {});
       galleryLightbox.refresh();
     })
     .catch(error => {
-      console.log(error);
+      iziToast.error({
+        message:
+          'Sorry, there was a problem with your request. Please try again!',
+        position: 'topRight',
+      });
     })
     .finally(() => {
       hideLoader();
